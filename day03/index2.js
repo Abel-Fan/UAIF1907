6767421f9a177fae8726efac253b2d8d1b28e4f8
Abel_fan <842615663@qq.com>
2019-07-31 18:22:00 +0800

    二维数组 深拷贝浅拷贝

--- day03/index2.js
+++ day03/index2.js
@@ -66,14 +66,35 @@ let category = ['书籍','衣服','电子产品']
 // console.log(num) // 123
 // console.log(arr) // [1,2,3]
 
-let arr = [1,2,3]
-let arr3 = []
+// let arr = [1,2,3]
+// let arr3 = []
 
 // 拷贝 浅拷贝~
-for(let i=0;i<arr.length;i++){
-    arr3[i]=arr[i]
-}
-console.log(arr3)
-arr3[2]="a"
-console.log(arr3)
-console.log(arr)
+// for(let i=0;i<arr.length;i++){
+//     arr3[i]=arr[i]
+// }
+// console.log(arr3)
+// arr3[2]="a"
+// console.log(arr3)
+// console.log(arr)
+
+
+// 二维数组
+// let arr = [[1,2],[3,4],[5,6]]
+// let arr = [[1,2],[3,4],[5,6]]
+//let arr1 = arr // 传址 
+// let arr2 = []
+
+// 深拷贝
+// for(let i=0;i<arr.length;i++){
+//     arr2[i] = arr[i] // arr[i] 引用类型
+// }
+
+// // console.log(arr2)
+// // console.log(arr2[1][0])
+// arr2[1][0]="aa"
+// console.log(arr)
+
+//二维数组的访问
+//二维数组的遍历
+//二维数组的基本操作
