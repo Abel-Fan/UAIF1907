9a7f60fd7a974f1a559c709100f70239fa803c75
Abel_fan <842615663@qq.com>
2019-08-01 16:36:12 +0800

    作用域 作用域链 let var区别 闭包函数

--- day04/index2.js
+++ day04/index2.js
@@ -37,3 +37,85 @@ function(){} //匿名函数
 //     console.log(sum)
 // }
 // add(10,30,50,70)
+
+// 作用域：变量作用的范围
+// 根据作用域不同，把变量分为全局变量,局部变量
+// 全局变量：在任何情况下都可以使用的变量
+// 局部变量：只能在块或者是函数内使用的称为局部变量
+
+// let aa = 123  //全局变量
+// function fun(){
+//     // let aa = 456
+//     console.log(aa)   // 1 456
+//     function fun2(){
+//         // let aa = 789
+//         console.log(aa) // 2 789
+//     }
+//     fun2()
+// }
+// fun()
+// console.log(aa)  // 3 123
+
+// 作用域链
+
+
+// ES5 函数作用域 function(){}
+// ES6 块级作用域  {}  ,只有let申明的变量才是块级作用域
+// var 6之前
+
+// var aa = 123
+// {
+//     var aa = 456
+// }
+// console.log(aa)
+
+// for(let i=0;i<10;i++){
+//     console.log(i)
+// }
+// // 0 1 2 3 4 5 6 7 8 9
+// console.log(i)  // 10
+
+
+
+
+//var let 区别
+//(1) let 支持块级作用域 var支持函数作用域
+//(2) var 支持变量提升(js加载时会经历 预解析（把var申明的变量以及function提前放到内存中）->解析 )
+//(3) let会出现暂时性死区
+
+// console.log(aa)
+// let aa = 10
+// console.log(aa)
+
+// console.log(aa)
+// var aa = 10
+// console.log(aa)
+
+// aa()
+// function aa(){
+//     console.log(123)
+// }
+
+
+// let aa = 123
+// function fun(){
+//     console.log(aa)  // 123  error : aa is not defined
+//     let aa = 456
+//     console.log(aa)  // 456
+// }
+// fun()
+
+// 名词解释
+// 闭包函数:两个嵌套的函数，内部函数使用外部函数的变量，然后在全局环境下调用内部函数时，形成闭包。用作与保护变量。
+function fun1(){
+    let aa = []
+    function fun2(data){
+        aa.push(data)
+        console.log(aa)
+    }
+    return fun2
+}
+let fun2 = fun1()
+fun2(123)
+fun2(456)
+fun2(789)
