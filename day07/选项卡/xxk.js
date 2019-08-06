eca799e3ada696b4dec437f3daff611beacd8f25
Abel_fan <842615663@qq.com>
2019-08-06 16:00:16 +0800

    选项卡效果

--- day07/选项卡/xxk.js
+++ day07/选项卡/xxk.js
@@ -4,11 +4,22 @@ window.onload=function(){
     let btns = document.querySelectorAll(".btns div")
     let cons = document.querySelectorAll(".cons div")
     let colors = ['pink','yellowgreen','orange']
-
+    cons[0].style.zIndex = 10
     btns.forEach((item,index)=>{
         item.style.backgroundColor = colors[index]
         cons[index].style.backgroundColor = colors[index]
+
+        item.onmouseenter = function(){
+            cons.forEach(function(item,index){
+                item.style.zIndex = 0
+            })
+            cons[index].style.zIndex = 10
+
+        }
     })
+
+    // 移入事件
+
 
 
 }
