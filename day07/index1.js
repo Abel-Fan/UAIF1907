5cdaec967fd7afd9e821e1cf83eb5486a58c5a45
Abel_fan <842615663@qq.com>
2019-08-06 09:11:12 +0800

    回顾

---
+++ day07/index1.js
@@ -0,0 +1,54 @@
+// //String
+// let str = "abc"
+// // 17
+// str.charAt()   // 返回某一位置的字符
+// str.charCodeAt()  // 返回某一位置的字符编码
+// String.fromCharCode()  // 将ASCII转化为字符串
+// str.indexOf()   // 返回第一次出现的下标
+// str.lastIndexOf() // 返回最后一次出现的下标
+// str.search()  // 检索指定字符 , 支持正则
+// str.match()  // 检索指定字符返回特殊数组 ,支持正则
+// str.repeat()
+// str.replace()
+// str.substr(start,length)
+// str.substring(start,end)
+// str.slice(start,end)
+// str.split()
+// str.toLowerCase()
+// str.toUpperCase()
+// str.trim()
+// str.startsWith()
+
+// //Array
+// let arr = [1,2,3,4,5,6]
+// arr.push()
+// arr.pop()
+// arr.unshift()
+// arr.shift()
+// arr.splice()
+// arr.slice()
+// arr.join()
+// arr.concat()
+// arr.sort()
+// arr.findIndex()
+// arr.find()
+// arr.forEach()
+// arr.map()
+// arr.reverse()
+// arr.includes()
+// arr.every()
+// arr.some()
+// arr.filter()
+
+// Math
+// Math.PI
+// Math.abs()
+// Math.round()
+// Math.ceil()
+// Math.floor()
+// Math.max()
+// Math.min()
+// Math.random()
+// Math.pow()
+// Math.sqrt()
+
