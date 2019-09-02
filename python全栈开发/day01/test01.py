50457e6059ebd5e7da56c0f971df4d6cce83c61f
Abel_fan <842615663@qq.com>
2019-09-02 11:13:21 +0800

    变量 数据类型数字 字符串

--- python全栈开发/day01/test01.py
+++ python全栈开发/day01/test01.py
@@ -39,3 +39,54 @@ a=b=c=1
 # print(a,b,c)
 # a=b=c="a"
 # print(a,b,c)
+
+# 变量的命名规则：
+"""
+1. 变量名由 字母数字下划线构成，数字不能开头，支持汉字
+2. 严格区分大小写
+3. 语义化
+4. 不能使用关键字和保留字
+"""
+
+# 数据类型
+# 1、数字类型
+# 整数int 浮点数float
+# 0b二进制  0o八进制 0x开头
+# 3e2
+
+# 2、字符串类型 str
+# 字符编码： ASCII GBK GB2312 UTF-8 unicode
+# 字符串定义： ''  ""   """ """   '''  '''
+# 转义字符：\'  \" \\  \t \r \n
+# 前缀
+#  r"abc"  取消转义
+#  u"abc"
+#  b"abc"
+#  f"abc"  模板字符串
+# name='小白'
+# print(f"{name} 你好")
+
+# 模板字符串
+# name = "小白"
+# print(f"{name}你好")
+# print("%s你好"%name)  # %s str
+# print("我有%d个手机"%10)           # %d 整型
+# print("day%2d"%10)
+# print("day%2d"%1)
+# print("价格为%0.2f元"%10)
+# print("%s你好，这里有%d个手机，一共价值%0.2f元"%("小白",10,10000))
+# 字符串访问
+# str1[index]  index可以为 负
+# print(str1[-1])
+# 切片
+# 语法 "abcdef"[start:end:step]
+str1 = "山西优逸客科技有限公司"
+# print(str1[2:5:1])
+# start 开始的下标，默认 0
+# end 结束前的下标  默认 length
+# step 步进值  可有可无 默认1
+# print(str1[::])
+# print(str1[-5:-1])
+# print(str1[-1:-5:-1])
+
+
