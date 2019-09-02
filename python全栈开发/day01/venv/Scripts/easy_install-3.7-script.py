c21d9614206e6ccf893f9dffb2415c27ce51cd3d
Abel_fan <842615663@qq.com>
2019-09-02 10:34:04 +0800

    python01

---
+++ python全栈开发/day01/venv/Scripts/easy_install-3.7-script.py
@@ -0,0 +1,12 @@
+#!D:\工作\带课班级资料\UIAIF1907\讲课内容\python全栈开发\day01\venv\Scripts\python.exe -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.7'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install-3.7')()
+    )
