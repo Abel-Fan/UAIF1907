0d82de329ee563be42570d65e0576283b12fab6b
Abel_fan <842615663@qq.com>
2019-09-03 10:47:52 +0800

    add

--- python全栈开发/day02/函数.py
+++ python全栈开发/day02/函数.py
@@ -81,3 +81,72 @@ def fn(a,b,c):
 # res = fn()
 # a,b,c,d,e = fn()
 # print(res,a,b,c,d,e)
+
+# 名词解释
+# 闭包
+# 两个发生嵌套关系的函数，内部函数使用外部函数的变量，在全局情况下使用内部函数时发生闭包。保护数据
+# def fn():
+#     arr = []
+#     def newFn(a):
+#         arr.append(a)
+#         print(arr)
+#     return newFn
+
+# newFn = fn()
+# newFn(1)
+# newFn(2)
+# newFn(3)
+# newFn(4)
+
+
+# 高阶函数  柯里化
+# mysum(1)(2)(3)
+
+# def mysum(a):
+#     def mysum2(b):
+#         def mysum3(c):
+#             return a+b+c
+#         return mysum3
+#     return mysum2
+#
+# res = mysum(10)(20)(30)
+# print(res)
+
+# 递归:
+# def jc(num):
+#     if num==1:
+#         return 1
+#     else:
+#         return num*jc(num-1)
+
+# def jc(num):
+#     return   1   if num==1 else  num*jc(num-1)
+
+# 装饰器
+# 功能： 给原函数添加新的功能
+# 特点，原则：1.不改变原函数   2.不改变原函数的调用方式  3.同时满足 1 and 2
+import time
+
+def fn():
+    time.sleep(1)
+    print("hello world")
+
+def fn1():
+    time.sleep(1)
+    print("hello")
+
+def fn2(fn):
+    def newFn():
+        start = time.time()
+        fn()
+        end = time.time()
+        print(f"总时间为：{end-start}")
+    return newFn
+
+# fn2(fn1)()
+# fn2(fn)()
+fn = fn2(fn)
+fn1 = fn2(fn1)
+
+fn()
+fn1()
