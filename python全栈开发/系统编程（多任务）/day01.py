75320abe81b07fdbfa16cbf458f1d7803f8cbf08
Abel_fan <842615663@qq.com>
2019-09-20 11:47:31 +0800

    递归锁 死锁 条件锁

--- python全栈开发/系统编程（多任务）/day01.py
+++ python全栈开发/系统编程（多任务）/day01.py
@@ -1,5 +1,5 @@
 # 线程是cpu运算最小的单元
-from threading import Thread,active_count,current_thread,enumerate,main_thread,stack_size
+from threading import Thread,active_count,current_thread,enumerate,main_thread,stack_size,Lock
 import time,random
 
 # print(active_count()) # 返回当前活动线程的数量
@@ -8,21 +8,107 @@ import time,random
 # print(main_thread())  # 返回当前主线程
 # print(stack_size()) # 返回创建线程时使用的栈的大小，如果指定size参数，则用来指定后续创建的线程使用的栈大小，size必须是0（表示使用系统默认值）或大于32K的正整数
 
-def task(num):
-    time.sleep(3)
-    print(num)
-start = time.time()
+# def task(num):
+#     time.sleep(3)
+#     print(num)
+# start = time.time()
+#
+# arr = []
+# for i in range(5):
+#     # task(i)
+#     t = Thread(target=task,args=(i,))
+#     t.start()
+#     arr.append(t)
+#
+# for t in arr:
+#     t.join()
+#
+# end = time.time()
+# print("运行总时间：%s" % (end - start))
 
-arr = []
-for i in range(5):
-    # task(i)
-    t = Thread(target=task,args=(i,))
-    t.start()
-    arr.append(t)
+# class Mytest(Thread):
+#     def __int__(self):
+#         super().__init__(self)
+#     def run(self):
+#         time.sleep(3)
+#         print(self.name)
+#
+# t1 = Mytest()
+# t1.start()
+
+"""
+Thread对象常用方法：
+start() 运行线程
+join(time) 阻塞主进程 time 阻塞时间
+name 线程的名字
+ident 线程的标识
+is_alive() 判断线程是否存活
+daemon 是否为守护线程 （守护线程：主线程不会等守护线程运行结束）
+"""
+
+# def task():
+#     time.sleep(2)
+#     print("task")
+#
+# t1 = Thread(target=task)
+# t1.daemon = True
+# t1.start()
+
+# class Mytast(Thread):
+#     def __init__(self):
+#         super(Mytast,self).__init__()
+#     def run(self):
+#         time.sleep(2)
+#         print(self.name)
+#
+# t1 = Mytast()
+# t1.daemon = True
+# t1.start()
 
 
-for t in arr:
-    t.join()
 
-end = time.time()
-print("运行总时间：%s" % (end - start))
+# Thread(group=None,target,args=())  构造函数
+"""
+group 默认为None 为扩展Thread保留
+target  线程启动执行函数
+args 是元组 是线程执行函数的参数
+kwargs  是线程执行函数的参数
+"""
+
+# 访问公共资源
+hg = []
+# 生产者消费者模式
+
+# 锁
+l1 = Lock()
+l2 = Lock()
+
+class Xm(Thread):
+    def __init__(self):
+        super(Xm,self).__init__()
+    def run(self):
+        while True:
+            l1.locked()
+            time.sleep(1)
+            hg.append(1)
+            print("小明放了1个鱼丸,锅内共有%s个鱼丸"%(len(hg)))
+            l1.release()
+
+class Xh(Thread):
+    def __init__(self):
+        super(Xh,self).__init__()
+    def run(self):
+        while True:
+            l1.acquire()
+            time.sleep(1)
+            hg.pop(0)
+            print("小红吃了1个鱼丸,锅内共有%s个鱼丸"%(len(hg)))
+            l1.release()
+
+
+
+xm = Xm()
+xm.start()
+xh = Xh()
+xh.start()
+
---
+++ python全栈开发/系统编程（多任务）/day02.py
@@ -0,0 +1,191 @@
+from threading import Thread,Lock
+import random
+#  锁的作用
+# arr = []
+#
+# #原始锁处于 "锁定" 或者 "非锁定" 两种状态之一。它被创建时为非锁定状态。它有两个基本方法， acquire() 和 release() 。当状态为非锁定时， acquire() 将状态改为 锁定 并立即返回。当状态是锁定时， acquire() 将阻塞至其他线程调用 release() 将其改为非锁定状态，然后 acquire() 调用重置其为锁定状态并返回。 release() 只在锁定状态下调用； 它将状态改为非锁定并立即返回。如果尝试释放一个非锁定的锁，则会引发 RuntimeError  异常。
+#
+# l = Lock()   # 状态默认是非锁定的
+# l.acquire()
+#
+# def test1():
+#     l.acquire()  # 如果之前的锁是锁定的，再次上锁发生阻塞
+#                  # 如果之前的锁是非锁定的，可以实现上锁
+#     arr.append(random.randint(1,10))
+#
+# t = Thread(target=test1)
+# t.start()
+#
+#
+#
+# def test2():
+#     arr.append(random.randint(1,0))
+
+
+
+"""
+爬取豆瓣电影top250
+"""
+# import requests
+# import lxml.etree as etree
+# urls = ["https://movie.douban.com/top250?start=%s"%i for i in range(0,226,25)]
+#
+# class Mytest(Thread):
+#     def __init__(self):
+#         super(Mytest,self).__init__()
+#     def run(self):
+#         while len(urls)>0:
+#             url = urls.pop()
+#             res = requests.get(url).text
+#             html = etree.HTML(res)
+#             titles = html.xpath("//div[@class='hd']/a/span[1]/text()")
+#             print(self.name,titles)
+#
+# for i in range(2):
+#     mytest = Mytest()
+#     mytest.start()
+
+"""
+向列表中插入元素并打印列表
+"""
+# import time
+# arr = []
+# l = Lock()
+# class Mytest(Thread):
+#     def __init__(self):
+#         super(Mytest,self).__init__()
+#     def run(self):
+#         time.sleep(1)
+#         l.acquire()
+#         arr.append(random.randint(0,10))
+#         print(arr)
+#         l.release()
+#
+# for i in range(20):
+#     t = Mytest()
+#     t.start()
+
+"""
+递归锁 复用锁 RLock()
+在一个线程内可以重复上锁不被阻塞。
+"""
+# from threading import RLock
+# import time
+# r = RLock()
+# l = Lock()
+# def test1():
+#     r.acquire()
+#     r.acquire()
+#     print("123")
+#     time.sleep(2)
+#     r.release()
+#     r.release()
+#
+# def test2():
+#     r.acquire()
+#     print('test2')
+#
+# t = Thread(target=test1)
+# t.start()
+#
+# t2 = Thread(target=test2)
+# t2.start()
+
+
+# 死锁
+# import time
+# l1 = Lock()
+# l2 = Lock()
+# def fun1():
+#     l1.acquire()
+#     print("fun1 is running")
+#     time.sleep(1)
+#     l2.acquire()   # 阻塞
+#     print("fun1 is end")
+#     l1.release()
+#     l2.acquire()
+# def fun2():
+#     l2.acquire()
+#     print("fun2 is running")
+#     l1.acquire() # 阻塞
+#     print("fun2 is end")
+#     l2.release()
+#     l1.release()
+#
+# t1 = Thread(target=fun1)
+# t1.start()
+# t2 = Thread(target=fun2)
+# t2.start()
+
+"""
+锁的上下文使用方法
+
+向列表中插入元素并打印列表
+"""
+# import time,random
+# arr = []
+# l = Lock()
+# def fun():
+#     time.sleep(1)
+#     with l:  # 上锁   自动解锁的功能
+#         arr.append(random.randint(0,10))
+#         print(arr)
+#
+# for i in range(20):
+#     t = Thread(target=fun)
+#     t.start()
+
+"""
+条件锁
+class Condition(lock=RLock)  默认是递归锁
+
+方法：
+acquire()  上锁
+release() 释放锁
+wait(timeout=) 等待 阻塞  知道 被通知
+wait_for(predicate,timeout=)  predicate 是一个函数 返回 True False
+notify(n=) 通知 唤醒n个线程
+
+notify_all() 唤醒所有线程
+"""
+from threading import Condition
+import time
+con = Condition()
+
+arr = []
+
+class XM(Thread):
+    def __init__(self):
+        super(XM,self).__init__()
+    def run(self):
+        con.acquire()
+        while True:
+            time.sleep(2)
+            arr.append(1)
+            length = len(arr)
+            print("小明添加了1个鱼丸,锅内还有%s个鱼丸"%length)
+            if length>=5:
+                con.wait()
+                con.notify()
+
+class XH(Thread):
+    def __init__(self):
+        super(XH,self).__init__()
+    def run(self):
+        con.acquire()
+        while True:
+            time.sleep(1)
+            arr.pop()
+            length = len(arr)
+            print("小红吃了1个鱼丸,锅内还有%s个鱼丸" % length)
+            if length<=0:
+                con.notify()
+                con.wait()
+xm = XM()
+xm.start()
+
+xh = XH()
+xh.start()
+
+
+
